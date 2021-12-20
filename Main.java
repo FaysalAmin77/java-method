7d3eb5bd4615e0be0f13bec17af756148ffd4a93
FaysalAmin77 <95224798+FaysalAmin77@users.noreply.github.com>
2021-12-20 09:13:21 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		faysal("ghsgdjh");
+		
+		
+	}
+	
+	static void faysal(Object amin){
+		    System.out.println(amin);
+		}
+		
+}
